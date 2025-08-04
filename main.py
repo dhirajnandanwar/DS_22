488b7532a47959302deb873c5d2badb53a7d5390
DHIRAJ <nandanward0@gmail.com>
2025-08-04 16:52:38 +0530

    fourth commit

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     print('welcome_to_bugspotter')
     print('dhiraj')
+    print('hello')
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
